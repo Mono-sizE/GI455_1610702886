4c06d49125d530d55bcba3b36bada7df36f06911
DESKTOP-7BGN970\admin <cholathep@gmail.com>
2021-01-29 23:20:09 +0700

    Sent Assignment Week 1

---
+++ GI4555_Project/Assets/TextMatching/Script/TextMatching.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class TextMatching : MonoBehaviour
+{
+    public Text WordText, ResultText;
+    public Button button;
+    public InputField SearchText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        WordText.text = "Unity\n Unreal\n Resident Evil\n Google\n MongoDB";
+    }
+    public void SearchingWord()
+    {
+        if (SearchText.text == "Unity" || SearchText.text == "Unreal" || SearchText.text == "Resident Evil" || SearchText.text == "Google" || SearchText.text == "MongoDB")
+        {
+            ResultText.text = SearchText.text + "is found!";
+        }
+        else
+        {
+            ResultText.text = "Try Again";
+        }
+    }
+
+}
